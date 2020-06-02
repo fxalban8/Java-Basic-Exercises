4e16c650ee51cf8f590b2fe559fa8b650503d05e
fxalban8 <francisco.alban@epn.edu.ec>
2020-06-02 12:44:07 -0500

    Creating a deployMenu function

--- src/eda_java_exercises/Main_Class.java
+++ src/eda_java_exercises/Main_Class.java
@@ -6,6 +6,15 @@ public class Main_Class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner (System.in);
+		
 	}
-
+	
+	
+	public static void deployMenu() {
+		System.out.println("1. N multiple of M");
+		System.out.println("2. Less or equal than X ");
+		System.out.println("3. Sum_Squares");
+	}
 }
+
+
